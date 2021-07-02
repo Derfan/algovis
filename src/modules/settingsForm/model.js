85c1fda6143c6515d27e6a33fbd1e17ef763a3ac
Borys Medvediev <medvediev.b@gmail.com>
2021-07-02 15:55:18 +0300

    add selectionSort

--- src/modules/settingsForm/model.js
+++ src/modules/settingsForm/model.js
@@ -3,7 +3,7 @@ export default class FormModel {
     this.elementsCount = 5;
     this.sortingTypeOptions = [
       { label: 'Bubble Sort', value: 'bubbleSort', disabled: false },
-      { label: 'Selection Sort', value: 'selectionSort', disabled: true },
+      { label: 'Selection Sort', value: 'selectionSort', disabled: false },
       { label: 'Insertion Sort', value: 'insertionSort', disabled: true },
       { label: 'Merge Sort', value: 'mergeSort', disabled: true },
       { label: 'Quick Sort', value: 'quickSort', disabled: true },
